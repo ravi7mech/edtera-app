6e567938d90f9760623b9c4a3c628e7532984cf5
Carlos Rucker <carlosvrucker@gmail.com>
2022-10-08 14:31:44 +0530

    Initial Commit

---
+++ apps/edtera-mobile/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'edtera-mobile',
+  webDir: '../../dist/apps/edtera-mobile',
+  bundledWebRuntime: false,
+};
+
+export default config;
